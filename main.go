28e5379bc7659439117c9015823fdb6f5d59bc86
Juha Lehtinen <jtlehtinen@fastmail.com>
2023-12-04 19:13:06 +0200

    fetch and display weather

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
 	"os"
 	"strings"
+	"time"
 )
+
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
 type options struct {
 	apiKey  string
@@ -18,6 +25,151 @@ type options struct {
 func exitWithError(errorMessage string) {
 	fmt.Fprintf(os.Stderr, "ERROR: %s\n", errorMessage)
 	os.Exit(1)
+}
+
+type Weather struct {
+	CityName    string
+	TimeZone    int
+	Visibility  float64
+	Temperature float64
+	Pressure    float64
+	Humidity    float64
+	WindSpeed   float64
+	WindDegrees float64
+	Conditions  string
+	Icon        string
+}
+
+func makeRequestURL(cityName, units, apiKey string) string {
+	cityName = url.QueryEscape(cityName)
+	apiKey = url.QueryEscape(apiKey)
+	return fmt.Sprintf("%s?q=%s&units=%s&appid=%s", BASE_URL, cityName, units, apiKey)
+}
+
+func fetchWeather(apiKey, cityName, units string) (*Weather, error) {
+	u := makeRequestURL(cityName, units, apiKey)
+
+	resp, err := http.Get(u)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("request status %d %s", resp.StatusCode, http.StatusText(resp.StatusCode))
+	}
+
+	// API docs: https://openweathermap.org/current
+	type response struct {
+		Weather []struct {
+			Main        string `json:"main"`
+			Description string `json:"description"`
+			Icon        string `json:"icon"`
+		} `json:"weather"`
+		Main struct {
+			Temperature float64 `json:"temp"`
+			Pressure    float64 `json:"pressure"`
+			Humidity    float64 `json:"humidity"`
+		} `json:"main"`
+		Wind struct {
+			Speed   float64 `json:"speed"`
+			Degrees float64 `json:"deg"`
+		} `json:"wind"`
+		Name       string  `json:"name"`
+		TimeZone   int     `json:"timezone"`
+		Visibility float64 `json:"visibility"`
+	}
+
+	var res response
+	err = json.NewDecoder(resp.Body).Decode(&res)
+	if err != nil {
+		return nil, err
+	}
+
+	w := &Weather{}
+	w.CityName = res.Name
+	w.TimeZone = res.TimeZone
+	w.Visibility = res.Visibility
+	w.Temperature = res.Main.Temperature
+	w.Pressure = res.Main.Pressure
+	w.Humidity = res.Main.Humidity
+	w.WindSpeed = res.Wind.Speed
+	w.WindDegrees = res.Wind.Degrees
+
+	// @NOTE: Maybe take all?
+	if len(res.Weather) > 0 {
+		w.Conditions = res.Weather[0].Description
+		w.Icon = res.Weather[0].Icon
+	}
+
+	return w, nil
+}
+
+func weatherIconToString(icon string) string {
+	// @TODO: Improve these...
+	// https://openweathermap.org/weather-conditions
+	switch icon {
+	case "01d":
+		return "☀️" // clear sky day
+	case "02d":
+		return "⛅" // few clouds day
+	case "03d":
+		return "☁️" // scattered clouds day
+	case "04d":
+		return "☁️" // broken clouds day
+	case "09d":
+		return "🌧️" // shower rain day
+	case "10d":
+		return "🌦️" // rain day
+	case "11d":
+		return "⛈️" // thunderstorm day
+	case "13d":
+		return "❄️" // snow day
+	case "50d":
+		return "🌫️" // mist day
+	case "01n":
+		return "🌑" // clear sky night
+	case "02n":
+		return "⛅" // few clouds night
+	case "03n":
+		return "☁️" // scattered clouds night
+	case "04n":
+		return "☁️" // broken clouds night
+	case "09n":
+		return "🌧️" // shower rain night
+	case "10n":
+		return "🌦️" // rain night
+	case "11n":
+		return "⛈️" // thunderstorm night
+	case "13n":
+		return "❄️" // snow night
+	case "50n":
+		return "🌫️" // mist night
+	default:
+		return ""
+	}
+}
+
+func display(w io.Writer, wt *Weather, opt *options) {
+	temperatureSymbol, windSpeedSymbol := "C", "m/s"
+	if opt.units == "imperial" {
+		temperatureSymbol, windSpeedSymbol = "F", "mi/h"
+	}
+
+	weatherSymbol := weatherIconToString(wt.Icon)
+
+	if opt.verbose {
+		t := time.Now().UTC().Add(time.Duration(wt.TimeZone) * time.Second)
+
+		fmt.Printf("%s %s\n", wt.CityName, t.Format(time.Stamp))
+		fmt.Printf("========================\n")
+		fmt.Printf("condition: %s %s\n", weatherSymbol, wt.Conditions)
+		fmt.Printf("temperature: %.0f°%s\n", wt.Temperature, temperatureSymbol)
+		fmt.Printf("pressure: %.0f hPa\n", wt.Pressure)
+		fmt.Printf("humidity: %.1f%%\n", wt.Humidity)
+		fmt.Printf("wind: %.0f° %.1f %s\n", wt.WindDegrees, wt.WindSpeed, windSpeedSymbol)
+	} else {
+		fmt.Printf("%s %0.f°%s %s %s\n", wt.CityName, wt.Temperature, temperatureSymbol, weatherSymbol, wt.Conditions)
+	}
 }
 
 func main() {
@@ -53,4 +205,11 @@ func main() {
 	if strings.TrimSpace(opt.city) == "" {
 		exitWithError("city name is required")
 	}
+
+	w, err := fetchWeather(opt.apiKey, opt.city, opt.units)
+	if err != nil {
+		exitWithError(err.Error())
+	}
+
+	display(os.Stdout, w, &opt)
 }
